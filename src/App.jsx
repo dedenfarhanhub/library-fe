4f96615fd22652c1689ba2ed029f47aeed021ad2
dedenfarhanhub <106763240+dedenfarhanhub@users.noreply.github.com>
2025-08-09 10:24:28 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+export default function App() {
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+            <h1 className="text-3xl font-bold text-blue-500">
+                React + Vite + Tailwind v4 + ESLint + Prettier 🚀
+            </h1>
+        </div>
+    );
+}
